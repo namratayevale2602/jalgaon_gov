87d66eb0e81ed79788b3ffdfe59ae0918888d773
bhoyepravin <bhoyepravin288@gmail.com>
2025-08-14 17:09:23 +0530

    update gallery

--- jalgaon-front/src/assets/index.js
+++ jalgaon-front/src/assets/index.js
@@ -20,6 +20,29 @@ import changdev from "../assets/Tourist/Changdev.jpeg";
 import girnadam from "../assets/Tourist/girna-dam.jpg";
 import patnadevi from "../assets/Tourist/patnadevi_temple.jpg";
 
+import meet1 from "../assets/Gallery/Meet1.jpeg";
+import meet2 from "../assets/Gallery/Meet2.jpeg";
+import meet3 from "../assets/Gallery/Meet3.jpeg";
+import meet4 from "../assets/Gallery/Meet4.jpeg";
+import meet5 from "../assets/Gallery/Meet5.jpeg";
+import meet6 from "../assets/Gallery/Meet6.jpeg";
+import meet7 from "../assets/Gallery/Meet7.jpeg";
+import meet8 from "../assets/Gallery/Meet8.jpeg";
+import meet9 from "../assets/Gallery/Meet9.jpeg";
+import meet10 from "../assets/Gallery/Meet10.jpeg";
+
+import opening1 from "../assets/Gallery/opening3.jpeg";
+import opening2 from "../assets/Gallery/openingc2.jpeg";
+
+import bhumipujan from "../assets/Gallery/bhumipujan1.jpeg";
+
+import ladki1 from "../assets/Gallery/ladki1.jpg";
+import ladki2 from "../assets/Gallery/ladki2.jpg";
+import ladki3 from "../assets/Gallery/ladki3.jpg";
+
+import womenemp1 from "../assets/Gallery/womenempower1.jpg";
+import womenemp2 from "../assets/Gallery/womenempower2.jpg";
+
 export {
   logo,
   Banner1,
@@ -39,4 +62,22 @@ export {
   changdev,
   girnadam,
   patnadevi,
+  meet1,
+  meet2,
+  meet3,
+  meet4,
+  meet5,
+  meet6,
+  meet7,
+  meet8,
+  meet9,
+  meet10,
+  opening1,
+  opening2,
+  bhumipujan,
+  ladki1,
+  ladki2,
+  ladki3,
+  womenemp1,
+  womenemp2,
 };
